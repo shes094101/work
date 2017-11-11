8db27bdb0d70c0748465a9c3fc84a0acbfbecd8d
shes094101 <32055777+shes094101@users.noreply.github.com>
2017-11-12 02:04:04 +0800

    work

--- src/Main9.java
+++ src/Main9.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main9 {
     public static void main(String[] args) {
         Scanner src = new Scanner(System.in);
-        float max=0;
+        float max=-2000000000;
         float min=2000000000;
         for (int i=0; i<10 ; i++){
             float x=src.nextInt();
